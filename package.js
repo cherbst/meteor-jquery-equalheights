3dcae71bab28716f87cf8e5f9d472c58bfaf5f76
Christoph Herbst <chris.p.herbst@gmail.com>
2014-10-29 12:00:50 +0100

    use versions from meteor 1.0

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'cherbst:jquery-equalheights',
   summary: 'jquery-equalheights packaged for Meteor',
-  version: '1.5.2_1',
+  version: '1.5.2_3',
   git: 'https://github.com/cherbst/meteor-jquery-equalheights.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
+  api.versionsFrom('METEOR@1.0');
   api.addFiles('jQuery.equalHeights/jquery.equalheights.js', 'client');
 });
